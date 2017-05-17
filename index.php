81ed8fce6fbd2de39a55df4a7ead4044a3bb45ab
Micheline Bourque <mbourque@mail.greenriver.edu>
2017-05-16 22:00:42 -0500

    Added templating variables

--- index.php
+++ index.php
@@ -10,9 +10,34 @@
     //Default route
     $f3->route('GET /', function($f3) {
         
-        echo "Hello!";
+        $f3->set('username', 'jshmo');
+        $f3->set('password', sha1('Password01'));
+        $f3->set('title', 'Working with templates');
+        $f3->set('temp', 68);
+        $f3->set('color', 'black');
+        $f3->set('radius', 10);
+        $f3->set('bookmarks', array('http://www.cnet.com',
+                                    'http://www.reddit.com',
+                                    'http://www.cnn.com'));
+        $f3->set('addresses', array('primary' =>
+                                  '1000 Apple ln Seattle, WA 98999',
+                                  'secondary' =>
+                                  '2510 100th Court Tac, WA 9000'));
+        
+        $f3->set('desserts', array('chocolate' =>
+                                   'Chocolate Mousse',
+                                   'vanilla' =>
+                                   'Vanilla Custard',
+                                   'strawberry' =>
+                                   'Strawberry Shortcake'
+                                  ));
+                                    
+        
+        echo Template::instance()->render('pages/info.html');
         
     });
+    
+    
 
     //Run fat free
     $f3->run();
---
+++ tmp/3iii34u5mtyc0.2hbmt1l6jeqsc.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+
+    </head>
+    <body>
+        <h1><?= $title ?></h1>
+        <p>User logged in a <?= $username ?> using <?= $password ?></p>
+        
+        <!-- working with espressions -->
+        <h3>Temperature</h3>
+        <p>Fahrenheit: <?= $temp ?> degrees</p>
+        <p>Celsius: <?= $temp - 32 * (5.0/9.0) ?> degrees</p>
+        <p>I like <?= $color ?>!</p>
+        <p>Circumference: <?= $radius * (2 * 3.14) ?></p>
+        
+        <!--looping over arrays-->
+        <h3>Bookmarks</h3>
+        <p>My first bookmark is <a href="<?= $bookmarks[0] ?>"><?= $bookmarks[0] ?></a></p>
+        <p>My second bookmark is <?= $bookmarks[1] ?></p>
+        
+        <ul>
+            <?php foreach (($bookmarks?:[]) as $bookmark): ?>
+                <li><a href="<?= $bookmark ?>"><?= str_replace('http://', '',
+                $bookmark); ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+        
+        <!--an associative array -->
+        <h3>Addresses</h3>
+        <p><?= $addresses['primary'] ?></p>
+        <p><?= $addresses['secondary'] ?></p>
+        
+        <!--looping over associative arrays -->
+        <?php foreach (($addresses?:[]) as $key=>$value): ?>
+            <p><?= $key ?> - <?= $value ?></p>
+        <?php endforeach; ?>
+        
+        <h3>Dessert Options</h3>
+        <?php foreach (($desserts?:[]) as $key=>$value): ?>
+            <input type='checkbox' value='<?= $key ?>'/><?= $value ?></br>
+        <?php endforeach; ?>    
+      
+ 
+        
+    </body>
+</html>
